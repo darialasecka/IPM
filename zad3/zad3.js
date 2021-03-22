4e82db29424a02d502558ab3aaf2ca0a3cdd89af
darialasecka <daria.las@wp.pl>
2021-03-22 10:56:13 +0100

    Added some collisions

--- zad3/zad3.js
+++ zad3/zad3.js
@@ -28,7 +28,7 @@ function generateBlock()
         h: 100, //height
         color: color,
         x: width - 200,
-        y:  200,
+        y: 200,
     });
 
     drawBlocks();
@@ -36,77 +36,92 @@ function generateBlock()
 
 function drawBlocks()
 {
-  var i, len = blocks.length;
-  for(i = 0; i < len; i++)
-  {
-    ctx.fillStyle = blocks[i].color;
-    ctx.fillRect(blocks[i].x, blocks[i].y, blocks[i].w, blocks[i].h);
-  }
+    var i, len = blocks.length;
+    for(i = 0; i < len; i++)
+    {
+      ctx.fillStyle = blocks[i].color;
+      ctx.fillRect(blocks[i].x, blocks[i].y, blocks[i].w, blocks[i].h);
+    }
 }
 
-    function mouseDown(e)
-    {
-    	var i, len = blocks.length;
-    	var bRect = canvas.getBoundingClientRect();
-    	mouseX = (e.clientX - bRect.left);
-    	mouseY = (e.clientY - bRect.top);
-    	for (i=0; i < len; i++) {
-    		if(inCircle(blocks[i], mouseX, mouseY)) 
-    		{
-    			drag = true;
-    			dragHoldX = mouseX - blocks[i].x;
-    			dragHoldY = mouseY - blocks[i].y;
-    			dragIndex = i;
-    		}
-    	}
-    	if (drag) 
-    	{
-    		window.addEventListener("mousemove", mouseMove, false);
-    	}
-    	canvas.removeEventListener("mousedown", mouseDown, false);
-    	window.addEventListener("mouseup", mouseUp, false);
-    	return false;		
+function mouseDown(e)
+{
+    var i, len = blocks.length;
+    var bRect = canvas.getBoundingClientRect();
+    mouseX = (e.clientX - bRect.left);
+    mouseY = (e.clientY - bRect.top);
+    for (i=0; i < len; i++) {
+      if(inBlock(blocks[i], mouseX, mouseY))
+      {
+        drag = true;
+        dragHoldX = mouseX - blocks[i].x;
+        dragHoldY = mouseY - blocks[i].y;
+        dragIndex = i;
+      }
     }
+    if (drag)
+    {
+      window.addEventListener("mousemove", mouseMove, false);
+    }
+    canvas.removeEventListener("mousedown", mouseDown, false);
+    window.addEventListener("mouseup", mouseUp, false);
+  return false;
+}
 
-    function mouseMove(e)
-    {
-    	var posX, posY;
-     
-    	var bRect = canvas.getBoundingClientRect();
-    	mouseX = (e.clientX - bRect.left);
-    	mouseY = (e.clientY - bRect.top);
-    	
-    	posX = mouseX - dragHoldX;
-    	posY = mouseY - dragHoldY;
-    	
-    	blocks[dragIndex].x = posX;
-    	blocks[dragIndex].y = posY;
-    	
-    	ctx.clearRect(0,0,canvas.width, canvas.height);
-    	drawPlatform();
-    	drawBlocks();
-    }
-    function mouseUp()
-    {
-    	canvas.addEventListener("mousedown", mouseDown, false);
-    	window.removeEventListener("mouseup", mouseUp, false);
-    	if (drag) 
-    	{
-    		drag = false;
-    		window.removeEventListener("mousemove", mouseMove, false);
-    	}
-    }
+function mouseMove(e)
+{
+    var posX, posY;
+    var minX = 0;
+    var maxX = canvas.width - 100;
+    var minY = 0;
+    var maxY = canvas.height - 100;
+
+    var bRect = canvas.getBoundingClientRect();
+    mouseX = (e.clientX - bRect.left);
+    mouseY = (e.clientY - bRect.top);
+
+    var platformW = width - 0.8*width - 100;
+    var platformH = height - 0.2*height - 100;
+
+    //screen border
+    posX = mouseX - dragHoldX;
+    posX = (posX < minX) ? minX : ((posX > maxX) ? maxX : posX);
+    posY = mouseY - dragHoldY;
+    posY = (posY < minY) ? minY : ((posY > maxY) ? maxY : posY);
+    //platform
+    posY = (posY < platformH) ? posY : platformH;
+    //blocks
+    
+
+
+
+    blocks[dragIndex].x = posX;
+    blocks[dragIndex].y = posY;
+
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawPlatform();
+    drawBlocks();
+}
+
+function mouseUp()
+{
     canvas.addEventListener("mousedown", mouseDown, false);
-
-    function inCircle(circle,mx,my)
+    window.removeEventListener("mouseup", mouseUp, false);
+    if (drag)
     {
-    	var imageData = ctx.getImageData(mx, my, 1, 1), index = (mx + my * imageData.width) * 4;
-    	if (imageData.data[3] > 0 && circle.color=="rgb(" + imageData.data[0] + "," + imageData.data[1] + "," + imageData.data[2] +")") 
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		return false;
-    	}
+      drag = false;
+      window.removeEventListener("mousemove", mouseMove, false);
     }
+}
+canvas.addEventListener("mousedown", mouseDown, false);
+
+function inBlock(block,mx,my)
+{
+    var imageData = ctx.getImageData(mx, my, 1, 1), index = (mx + my * imageData.width) * 4;
+    if (imageData.data[3] > 0 && block.color=="rgb(" + imageData.data[0] + "," + imageData.data[1] + "," + imageData.data[2] +")") {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
