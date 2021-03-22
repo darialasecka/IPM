3e83b0c78896b7820c024487bbf31208b9cd8859
darialasecka <daria.las@wp.pl>
2021-03-22 10:17:18 +0100

    Generating blocks

--- zad3/zad3.js
+++ zad3/zad3.js
@@ -24,6 +24,26 @@ function drawPlatform(){
 function generateBlock()
 {
     console.log("klocek");
+    var color = "rgb(" + Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255) +")";
+    blocks.push({
+        w: 100, //width
+        h: 100, //height
+        color: color,
+        posX: width - 200,
+        posY:  200,
+    });
+
+    drawBlocks();
+}
+
+function drawBlocks()
+{
+  var i, len = blocks.length;
+  for(i = 0; i < len;i++)
+  {
+    ctx.fillStyle = blocks[i].color;
+    ctx.fillRect(blocks[i].posX, blocks[i].posY, blocks[i].w, blocks[i].h)
+  }
 }
 
 
