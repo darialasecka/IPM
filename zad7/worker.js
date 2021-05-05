9aff66875d0b814003b7016713ec546a492945f0
darialasecka <daria.las@wp.pl>
2021-05-05 10:02:11 +0200

    Added inverse texts in worker and changed sending data to worker from db to form

--- zad7/worker.js
+++ zad7/worker.js
@@ -1,3 +1,17 @@
 onmessage = function (e) {
-    console.log(JSON.parse(e.data));
+  var data = JSON.parse(e.data);
+  console.log(data);
+
+  for (var i = 0, len = data.length; i < len; i++) {
+    var character = data[i];
+    if (character == character.toLowerCase()) {
+      // The character is lowercase
+      output = output + character.toUpperCase();
+    } else {
+      // The character is uppercase
+      output = output + character.toLowerCase();
+    }
+  }
+  var output = '';
+  console.log(output)
 };
