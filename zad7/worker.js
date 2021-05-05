707965d2ddbc9cc01036c7f046b4b45e259f753c
darialasecka <daria.las@wp.pl>
2021-05-05 10:33:21 +0200

    Fixed , to ;

--- zad7/worker.js
+++ zad7/worker.js
@@ -4,20 +4,20 @@ onmessage = function (e) {
 
   for(var key in cuontact){
     contact[key] = reverseText(contacy[key]);
-  };
+  }
 
   console.log('Worker finished');
   postMessage(contact)
 };
 
 function reverseText(text) {
-  for (var i = 0, i < text.length; i++) {
+  for (var i = 0; i < text.length; i++) {
     var character = text[i];
     if (character == character.toLowerCase()) { // The character is lowercase
       output += character.toUpperCase();
     } else { // The character is uppercase
       output += character.toLowerCase();
     }
-  };
+  }
   return output
 }
